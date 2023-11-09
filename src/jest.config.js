53ed2d7f85c1f92b581be9c893b7f2ec9dbd7faa
Siris <sirish@gritfeat.com>
2023-11-09 17:16:31 +0545

    Update Config

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    projects: [
-      {
-        displayName: "Unit Tests",
-        testMatch: ["<rootDir>/**/__tests__/**/*.js"],
-        testPathIgnorePatterns: [
-          '<rootDir>/node_modules/',
-          '<rootDir>/__tests__/lib'
-        ],
-        testEnvironment: 'node',
-      },
-    ],
-    reporters: ['default', '.']
-  };
-  
+  projects: [
+    {
+      displayName: "Unit Tests",
+      testMatch: ["<rootDir>/**/__tests__/**/*.js"],
+      testPathIgnorePatterns: [
+        "<rootDir>/node_modules/",
+        "<rootDir>/__tests__/lib",
+      ],
+      testEnvironment: "node",
+    },
+  ],
+  reporters: ["default", "."],
+};
