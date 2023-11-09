8e73d530c6d070393ec47c21bdace862e2495204
Siris <sirish@gritfeat.com>
2023-11-09 17:14:07 +0545

    Add config file

---
+++ src/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    projects: [
+      {
+        displayName: "Unit Tests",
+        testMatch: ["<rootDir>/**/__tests__/**/*.js"],
+        testPathIgnorePatterns: [
+          '<rootDir>/node_modules/',
+          '<rootDir>/__tests__/lib'
+        ],
+        testEnvironment: 'node',
+      },
+    ],
+    reporters: ['default', '.']
+  };
+  
